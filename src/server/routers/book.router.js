d8e42bb56de8537a7c1d5b290e37b8972423d76d
Mohamed Aharrat <mohamedaharrat1@gmail.com>
2021-12-03 14:29:20 +0000

    Revert "books can be created with /create post request"
    This reverts commit afe196dce947b923bd0246a0e5ede7150567ce32

--- src/server/routers/book.router.js
+++ src/server/routers/book.router.js
@@ -8,36 +8,9 @@ const router = Express.Router()
 
 // User Schema
 const Book = require('../models/book.model')
-const Room = require('../models/room.model')
 
-// Return all Books, that has a room assignet to it
 router.get("/", async (req , res) => {
-    try {
-        const allBook = await Book.find()
-        console.log(allBook[8]._id)
-        
-        res.status(200).json(allBook)
-    } catch (error) {
-        res.status(500).send({message: error.message})
-    }
-})
-
-router.post("/create", async (req, res) => {
-    const { title, author, cover} = req.body;
-
-    const book = new Book({
-        title: title,
-        author: author,
-        cover: cover
-    })
-
-    try {
-        const newBook = await book.save()
-        // Succesfully created object
-        res.status(201).send({ok: 1, message: "Book has been created"})
-    } catch (error) {
-        res.status(400).send({ok: 0, message: error.message})
-    }
+    res.json({message: 'A GET end point, returning all the books that have rooms.'})
 })
 
 module.exports = router
